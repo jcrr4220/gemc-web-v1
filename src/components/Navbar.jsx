a9a8eabf07c37142fd4295dcd9d71acfcad31b5a
Joao Rocha <jcrrocha@gmail.com>
2022-10-02 11:29:28 +0100

    Update 02/10/2022

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import {Link, NavLink} from 'react-router-dom'
 import Logo from '../images/logo.png'
 import {links} from '../data'
-import {GoThreeBars} from 'react-icons/go'
+import {FiMenu} from 'react-icons/fi'
 import {MdOutlineClose} from 'react-icons/md'
 import './navbar.css'
 
@@ -28,7 +28,7 @@ const Navbar = () => {
             </ul>
             <button className="nav__toggle-btn" onClick={() => setIsNavShowing(prev => !prev)}>
                 {
-                  isNavShowing ? <MdOutlineClose/> : <GoThreeBars/>
+                  isNavShowing ? <MdOutlineClose/> : <FiMenu/>
                 }
             </button>
         </div>
