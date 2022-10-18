fe08ff81ae4ab3d026d58cca879465fb36b652fe
jcrr4220 <jcrrocha@gmail.com>
2022-10-18 16:43:41 +0100

    20221018v2

---
+++ src/components/Values.jsx
@@ -0,0 +1,18 @@
+import Image from '../images/values.jpg'
+import SectionHead from './SectionHead'
+import {GiCutDiamond} from 'react-icons/gi'
+import {values} from '../data'
+import Card from '../UI/Card'
+
+
+const Values = () => {
+  return (
+    <section className='values'>
+        <div className="container values__container">
+            Estamos a começar !...
+        </div>
+    </section>
+  )
+}
+
+export default Values
