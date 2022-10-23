40e760fbfebb4c42b6036c1dba294aa80ade3777
Joao Rocha <jcrrocha@gmail.com>
2022-10-23 11:58:49 +0100

    Update 20221023

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,8 +1,34 @@
+import Header from '../../components/Header'
+import HeaderImage from '../../images/header_bg_3.jpg'
+
 import './gallery.css'
 
 const Gallery = () => {
+  const galleryLength = 15;
+  const images = []
+
+  for (let i = 1; i <= galleryLength; i++) {
+    images.push(require(`../../images/gallery${i}.jpg`))
+  }
+
+
   return (
-    <div>Gallery</div>
+    <>
+    <Header title="A Nossa Galeria" image={HeaderImage}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit magnam sit numquam blanditiis nisi explicabo.
+    </Header>
+    <section className="gallery">
+      <div className="container gallery__container">
+        {
+          images.map((image, index) => {
+            return <article key={index}>
+              <img src={image} alt={`Gallery Img ${index + 1}`} />
+            </article>
+          })
+        }
+      </div>
+    </section>
+    </>
   )
 }
 
--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,8 +1,15 @@
+import {Link} from 'react-router-dom'
+
 import './notFound.css'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <section>
+      <div className="container notfound__container">
+        <h2>Página Inexistente!</h2>
+        <Link to="/" className='btn'>Voltamos ao Inicio</Link>
+      </div>
+    </section>
   )
 }
 
